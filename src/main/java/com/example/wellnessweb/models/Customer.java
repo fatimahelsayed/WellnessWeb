8437b2b048709202d22a6fb408fcdf47232812b5
janahani <Jana2100237@miuegypt.edu.eg>
2024-04-28 11:36:04 +0200

    Customer model and repository

---
+++ src/main/java/com/example/wellnessweb/models/Customer.java
@@ -0,0 +1,81 @@
+package com.example.wellnessweb.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int ID;
+    private String Name;
+    private int Age;
+    private String Gender;
+    private String Email;
+    private String Password;
+
+
+    public Customer() {
+    }
+
+    public Customer(int ID, String Name, int Age, String Gender, String Email, String Password) {
+        this.ID = ID;
+        this.Name = Name;
+        this.Age = Age;
+        this.Gender = Gender;
+        this.Email = Email;
+        this.Password = Password;
+    }
+
+    public int getID() {
+        return this.ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return this.Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public int getAge() {
+        return this.Age;
+    }
+
+    public void setAge(int Age) {
+        this.Age = Age;
+    }
+
+    public String getGender() {
+        return this.Gender;
+    }
+
+    public void setGender(String Gender) {
+        this.Gender = Gender;
+    }
+
+    public String getEmail() {
+        return this.Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getPassword() {
+        return this.Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+    
+}
