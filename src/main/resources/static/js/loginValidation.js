41b62b867cbb9f5157efdc6c546a1d4c993a94fa
janahani <Jana2100237@miuegypt.edu.eg>
2024-04-29 14:28:46 +0200

    login validation works

--- src/main/resources/static/js/loginValidation.js
+++ src/main/resources/static/js/loginValidation.js
@@ -1,6 +1,7 @@
 function validateForm() {
     var username = document.getElementById("username").value.trim();
     var password = document.getElementById("password").value.trim();
+    var i = true;
   
     // Clear existing error messages
     var errorElements = document.querySelectorAll('.error-message');
@@ -11,16 +12,16 @@ function validateForm() {
     // Validate username
     if (username === "") {
       document.getElementById("username-error").innerText = "Please enter your username";
-      return false;
+      i=false;
     }
   
     // Validate password
     if (password === "") {
       document.getElementById("password-error").innerText = "Please enter your password";
-      return false; 
+      i=false; 
     }
   
-    return true; 
+    return i; 
   }
 
 
