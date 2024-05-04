eb63ea955491b4d8036239d6d77c7b355a4bf6df
janahani <Jana2100237@miuegypt.edu.eg>
2024-05-04 10:31:52 +0200

    .

--- src/main/java/com/example/wellnessweb/controllers/AdminController.java
+++ src/main/java/com/example/wellnessweb/controllers/AdminController.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
