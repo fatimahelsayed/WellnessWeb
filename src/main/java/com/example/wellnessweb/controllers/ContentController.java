c76fd137e484c4f53c0b851a1e161040d9b31e09
malakHelmy <121347791+malakHelmy@users.noreply.github.com>
2024-05-05 16:38:00 +0300

    view content almost done missing subtopics

--- src/main/java/com/example/wellnessweb/controllers/ContentController.java
+++ src/main/java/com/example/wellnessweb/controllers/ContentController.java
@@ -3,7 +3,9 @@ package com.example.wellnessweb.controllers;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -26,6 +28,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequestMapping("/content")
@@ -49,6 +52,24 @@ public class ContentController {
         mav.addObject("articleObj", articleObj);
         return mav;
     }
+    public static String convertToBase64(byte[] imageData) {
+        return Base64.getEncoder().encodeToString(imageData);
+    }
+
+
+    @GetMapping("viewContent/{id}")
+    public ModelAndView getBlogById(@PathVariable("id") int id) {
+        ModelAndView mav = new ModelAndView("content.html");
+        Content contentObj = contentRepository.findById(id).orElse(null);
+        if (contentObj != null) {
+            byte[] image = contentObj.getImage();
+            String base64Image = (image != null) ? Base64.getEncoder().encodeToString(image) : "";
+            mav.addObject("contentObj", contentObj);
+            mav.addObject("base64Image", base64Image);
+        }
+        return mav;
+    }
+    
 
     @Transactional
     @PostMapping("addContent")
